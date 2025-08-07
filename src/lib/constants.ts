bac116f1752e9b23f2d25faea742e2e37e8e38f6
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-08-07 22:55:34 +0000

    Initial commit from template

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const PROJECT_TITLE = "$stfu-game";
+export const PROJECT_DESCRIPTION = "by @niftyassets";
+export const PROJECT_CREATOR = "niftyassets";
+export const PROJECT_AVATAR_URL =
+  "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
