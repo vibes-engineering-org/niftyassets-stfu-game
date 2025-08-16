0305667f478a2066a1ed3048fe1a7c43112d27dd
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-08-16 17:08:47 +0000

    Code generation: 07edf325

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_TITLE = "$stfu-game";
-export const PROJECT_DESCRIPTION = "by @niftyassets";
+export const PROJECT_DESCRIPTION = "Test your Web3 knowledge with true/false quiz statements about crypto and Ethereum";
 export const PROJECT_CREATOR = "niftyassets";
 export const PROJECT_AVATAR_URL =
   "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
